31e98e213096c6707c1c9f5b0c2ae83c2c807f97
Nikoali Makarov <makarov.nikolai20@gmail.com>
2024-05-28 14:54:08 +0300

    test for s3imagecollector were added

---
+++ tests/test_data_collectors/test_s3_image_collector.py
@@ -0,0 +1,117 @@
+import glob
+import pathlib
+
+import numpy as np
+import pandas as pd
+import pytest
+from PIL import Image
+
+from src.preprocessing_pipeline.s3_collectors import image_collector
+
+cwd: pathlib.Path = pathlib.Path(__file__).parent.resolve()
+
+
+test_data = (
+    ('b-ws-faq3m-mp1', 'preprocessing_pipeline_test_dir_nested/'),
+    ('b-ws-faq3m-mp1', 'preprocessing_pipeline_test_dir_nested'),
+)
+
+
+@pytest.mark.parametrize('bucket_name,folder_name', test_data)
+def test_nested_structure_dir_get_url_and_name(bucket_name: str, folder_name: str):
+    img_collector: image_collector.S3ImagesCollector = image_collector.S3ImagesCollector(
+        cwd.joinpath('crowd_dump.cfg'),
+    )
+
+    urls_and_names: pd.DataFrame = img_collector.get_url_and_name(
+        folder_name=folder_name,
+        bucket_name=bucket_name,
+    )
+
+    answer: pd.DataFrame = pd.read_csv(cwd.joinpath(f'./dumps/url_and_name/{folder_name.rstrip("/")}.csv'))
+
+    assert urls_and_names.equals(answer)
+
+
+test_data = (
+    ('b-ws-faq3m-mp1', 'preprocessing_pipeline_test_dir_flat/'),
+    ('b-ws-faq3m-mp1', 'preprocessing_pipeline_test_dir_flat'),
+)
+
+
+@pytest.mark.parametrize('bucket_name,folder_name', test_data)
+def test_flat_structure_dir_get_url_and_name(bucket_name: str, folder_name: str):
+    img_collector: image_collector.S3ImagesCollector = image_collector.S3ImagesCollector(
+        cwd.joinpath('crowd_dump.cfg'),
+    )
+
+    urls_and_names: pd.DataFrame = img_collector.get_url_and_name(
+        folder_name=folder_name,
+        bucket_name=bucket_name,
+    )
+
+    answer: pd.DataFrame = pd.read_csv(cwd.joinpath(f'./dumps/url_and_name/{folder_name.rstrip("/")}.csv'))
+
+    assert urls_and_names.equals(answer)
+
+
+test_data = (
+    ('b-ws-faq3m-mp1', 'preprocessing_pipeline_test_dir_nested/'),
+    ('b-ws-faq3m-mp1', 'preprocessing_pipeline_test_dir_nested'),
+)
+
+
+@pytest.mark.parametrize('bucket_name,folder_name', test_data)
+def test_nested_structure_dir_get_image_and_name(bucket_name: str, folder_name: str):
+    img_collector: image_collector.S3ImagesCollector = image_collector.S3ImagesCollector(
+        cwd.joinpath('crowd_dump.cfg'),
+    )
+
+    images_and_names: list[tuple[np.ndarray, str]] = img_collector.get_image_and_name(
+        folder_name=folder_name,
+        bucket_name=bucket_name,
+    )
+    images_and_names = sorted(images_and_names, key=lambda x: x[1])
+
+    answer_images: list[str] = glob.glob(str(cwd.joinpath('dumps/image_and_name/nested/**/*.JPG')), recursive=True)
+    answer_images = sorted(answer_images)
+
+    for i in range(len(answer_images)):
+        image: np.ndarray = np.asarray(Image.open(answer_images[i]))
+        filename: str = '/'.join(answer_images[i].replace('\\', '/').split('/')[-2:])
+
+        assert np.allclose(images_and_names[i][0], image)
+        assert images_and_names[i][1] == filename
+
+
+test_data = (
+    ('b-ws-faq3m-mp1', 'preprocessing_pipeline_test_dir_flat/'),
+    ('b-ws-faq3m-mp1', 'preprocessing_pipeline_test_dir_flat'),
+)
+
+
+@pytest.mark.parametrize('bucket_name,folder_name', test_data)
+def test_flat_structure_dir_get_image_and_name(bucket_name: str, folder_name: str):
+    img_collector: image_collector.S3ImagesCollector = image_collector.S3ImagesCollector(
+        cwd.joinpath('crowd_dump.cfg'),
+    )
+
+    images_and_names: list[tuple[np.ndarray, str]] = img_collector.get_image_and_name(
+        folder_name=folder_name,
+        bucket_name=bucket_name,
+    )
+    images_and_names = sorted(images_and_names, key=lambda x: x[1])
+
+    answer_images: list[str] = glob.glob(str(cwd.joinpath('dumps/image_and_name/flat/**/*.JPG')), recursive=True)
+    answer_images = sorted(answer_images)
+
+    for i in range(len(answer_images)):
+        image_answer = np.asarray(Image.open(answer_images[i]))
+
+        images_from_s3_np: np.ndarray = images_and_names[i][0]
+
+        filename: str = answer_images[i].replace('\\', '/').split('/')[-1]
+
+        assert images_from_s3_np.shape == image_answer.shape
+        assert np.allclose(images_from_s3_np, image_answer)
+        assert images_and_names[i][1] == filename
